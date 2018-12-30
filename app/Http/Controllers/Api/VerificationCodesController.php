ebca70dce0ee451ea28a85dbc6518add6a417ed4
yll1024335892 <yll1024335892@163.com>
2018-12-30 15:43:18 +0800

    发送验证码的api接口写完

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\VerificationCodeRequest;
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request,EasySms $easySms)
+    {
+        $phone=$request->phone;//获取手机号
+        $code=str_pad(random_int(1,99999),5,0,STR_PAD_LEFT);
+        if(!app()->environment('production')){
+            $code="12345";
+        }else{//现实版才发送验证码
+            try {
+                $result = $easySms->send($phone, [
+                    'content'  =>  "【阿翼网】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('yunpian')->getMessage();
+                return $this->response->errorInternal($message ?: '短信发送异常');
+            }
+        }
+        $key = 'verificationCode_'.str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+
+    public  function store1(){
+
+
+    }
+}
