7937aeb6691f5545f920c4527a024d9ef76b650a
yll1024335892 <yll1024335892@163.com>
2019-01-01 10:31:05 +0800

    api的图片验证码的添加

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,12 +10,21 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request,EasySms $easySms)
     {
-        $phone=$request->phone;//获取手机号
-        $code=str_pad(random_int(1,99999),5,0,STR_PAD_LEFT);
+        $captchData=\Cache::get($request->captcha_key);
+        if(!$captchData){
+            return $this->response->error("图片验证码失效",422);
+        }
+        if(!hash_equals($captchData['code'],$request->captcha_code)){
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized("验证码错误");
+        }
+
+        $phone=$captchData['phone'];//获取手机号
         if(!app()->environment('production')){
             $code="12345";
         }else{//现实版才发送验证码
             try {
+                $code=str_pad(random_int(1,99999),5,0,STR_PAD_LEFT);
                 $result = $easySms->send($phone, [
                     'content'  =>  "【阿翼网】您的验证码是{$code}。如非本人操作，请忽略本短信"
                 ]);
@@ -28,6 +37,8 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        //清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
         return $this->response->array([
             'key' => $key,
             'expired_at' => $expiredAt->toDateTimeString(),
--- readme.md
+++ readme.md
@@ -17,11 +17,13 @@ composer require monolog/monolog   日志系统
 
 spatie/laravel-permission		权限管理扩展
 
-mews/captcha	验证码
+mews/captcha	验证码针对pc
 
 dingo/api   api的一个好的工具
 
 overtrue/easy-sms 短信发布组建
+
+gregwar/captcha  验证码针对api
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
